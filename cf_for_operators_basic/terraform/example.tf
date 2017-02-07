6b6f649ca0e156094dc65b18b8f5a7b93efc5e6b
Vladimir Sagan <vlasad@gmail.com>
2017-02-07 17:28:49 +0300

    a

--- cf_for_operators_basic/terraform/example.tf
+++ cf_for_operators_basic/terraform/example.tf
@@ -76,6 +76,25 @@ resource "aws_subnet" "vpc_public_subnet" {
     cidr_block = "10.0.0.0/24"
 }
 
+resource "aws_internet_gateway" "vpc_internet_gateway" {
+    vpc_id = "${aws_vpc.training_vpc.id}"
+}
+
+resource "aws_route_table" "vpc_public_subnet_router" {
+    vpc_id = "${aws_vpc.training_vpc.id}"
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.vpc_internet_gateway.id}"
+    }
+
+}
+
+resource "aws_route_table_association" "vpc_public_subnet_router_association" {
+    subnet_id = "${aws_subnet.vpc_public_subnet.id}"
+    route_table_id = "${aws_route_table.vpc_public_subnet_router.id}"
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-0ee2276e"
   instance_type = "t2.micro"
