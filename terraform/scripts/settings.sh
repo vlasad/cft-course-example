e6c934c0d640c2a5d3477941a38f35a76cd1f91c
Vladimir Sagan <vlasad@gmail.com>
2017-02-22 18:03:05 +0300

    new terraform

---
+++ terraform/scripts/settings.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+sudo bash -c 'echo "PermitUserEnvironment yes" >> /etc/ssh/sshd_config'
+sudo bash -c 'echo "MaxSessions 1000" >> /etc/ssh/sshd_config'
+sudo sed -i 's/AcceptEnv .*/AcceptEnv \*/g' /etc/ssh/sshd_config
+sudo service ssh restart
---
+++ terraform/variables.tf
@@ -0,0 +1,35 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "platform_public_key" {}
+variable "platform_private_key_path" {}
+
+variable "owner_tag" {
+    description = "User email from platform"
+}
+
+variable "name_tag" {
+    description = "Name of infrastructure from platform"
+}
+
+variable "jumpbox_user" {
+    default = "vcap"
+}
+
+variable "ssh_port" {
+    default = "22"
+}
+
+variable "vpc_cidr" {
+    description = "CIDR for the whole VPC"
+    default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+    description = "CIDR for the Public Subnet"
+    default = "10.0.0.0/24"
+}
+
+variable "uuid" {
+    description = "Unique prefix for all resources"
+    default = "${uuid()}"
+}
