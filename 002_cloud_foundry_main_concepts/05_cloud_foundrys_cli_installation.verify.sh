88eb86c4d8213ab4d4082c6c8444cd6e0965a65c
Vladimir S <vlasad@gmail.com>
2017-02-22 19:31:49 +0300

    Update 05_cloud_foundrys_cli_installation.verify.sh

--- 002_cloud_foundry_main_concepts/05_cloud_foundrys_cli_installation.verify.sh
+++ 002_cloud_foundry_main_concepts/05_cloud_foundrys_cli_installation.verify.sh
@@ -1,28 +1,3 @@
 #!/bin/bash -e
 
 check 'cf --version' 'cf version 6\.*' true #> Cloud Foundry CLI not installed or version outdated.
-check 'cf --version' 'cf version 6\.*' true #> Cloud Foundry CLI not installed or version outdated.
-check 'cf --version' 'cf version 6\.*' true #> Cloud Foundry CLI not installed or version outdated.
-check 'cf --version' 'cf version 6\.*' true #> Cloud Foundry CLI not installed or version outdated.
-check 'cf --version' 'cf version 6\.*' true #> Cloud Foundry CLI not installed or version outdated.
-check 'cf --version' 'cf version 6\.*' true #> Cloud Foundry CLI not installed or version outdated.
-check 'cf --version' 'cf version 6\.*' true #> Cloud Foundry CLI not installed or version outdated.
-check 'cf --version' 'cf version 6\.*' true #> Cloud Foundry CLI not installed or version outdated.
-check 'cf --version' 'cf version 6\.*' true #> Cloud Foundry CLI not installed or version outdated.
-check 'cf --version' 'cf version 6\.*' true #> Cloud Foundry CLI not installed or version outdated.
-check 'cf --version' 'cf version 6\.*' true #> Cloud Foundry CLI not installed or version outdated.
-check 'cf --version' 'cf version 6\.*' true #> Cloud Foundry CLI not installed or version outdated.
-check 'cf --version' 'cf version 6\.*' true #> Cloud Foundry CLI not installed or version outdated.
-check 'cf --version' 'cf version 6\.*' true #> Cloud Foundry CLI not installed or version outdated.
-check 'cf --version' 'cf version 6\.*' true #> Cloud Foundry CLI not installed or version outdated.
-check 'cf --version' 'cf version 6\.*' true #> Cloud Foundry CLI not installed or version outdated.
-check 'cf --version' 'cf version 6\.*' true #> Cloud Foundry CLI not installed or version outdated.
-check 'cf --version' 'cf version 6\.*' true #> Cloud Foundry CLI not installed or version outdated.
-check 'cf --version' 'cf version 6\.*' true #> Cloud Foundry CLI not installed or version outdated.
-check 'cf --version' 'cf version 6\.*' true #> Cloud Foundry CLI not installed or version outdated.
-check 'cf --version' 'cf version 6\.*' true #> Cloud Foundry CLI not installed or version outdated.
-check 'cf --version' 'cf version 6\.*' true #> Cloud Foundry CLI not installed or version outdated.
-check 'cf --version' 'cf version 6\.*' true #> Cloud Foundry CLI not installed or version outdated.
-check 'cf --version' 'cf version 6\.*' true #> Cloud Foundry CLI not installed or version outdated.
-check 'cf --version' 'cf version 6\.*' true #> Cloud Foundry CLI not installed or version outdated.
-check 'cf --version' 'cf version 6\.*' true #> Cloud Foundry CLI not installed or version outdated.
